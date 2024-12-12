df704733cee6f67589d0d72cc35f5f7855e029fc
saito4 <hiroto.saitou1201@gmail.com>
2024-12-12 20:40:35 +0900

    Euclid's mutual division method

---
+++ algorithm2.py
@@ -0,0 +1,31 @@
+my_list = [1, 3, 2, 5, 4, 9]
+
+max_valu = my_list[0]
+for val in my_list[1:]:
+    if max_valu < val:
+        max_valu = val
+
+print("最大値：", max_valu)
+
+# ユークリッドの互除法 どこで終わるのかが繰り返し処理中に判明するような処理はwhileを使うのがいい
+
+def my_gcd(a, b):
+    if b == 0:
+        return a
+    
+    m = a % b
+    return my_gcd(b, m)
+
+x = my_gcd(144, 84)
+print(x)
+
+def gcd(c, d):
+    while d != 0:
+        gcd = d
+        d = c % d
+        c = gcd
+    
+    return gcd
+
+y = gcd(144, 84)
+print(y)
