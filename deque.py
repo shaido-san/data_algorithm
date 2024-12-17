4dc39fba2960edd07983dcb055282e4d3ab7ada3
saito4 <hiroto.saitou1201@gmail.com>
2024-12-17 15:07:34 +0900

    stack

---
+++ deque.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+#python標準の双方向リスト
+
+my_deque = deque()
+my_deque.append(1)
+my_deque.append(2)
+my_deque.append(3)
+
+print(my_deque[1])
+x = my_deque.pop()
+print(x)
+del my_deque[0]
+print(my_deque)
---
+++ stack.py
@@ -0,0 +1,34 @@
+class MyStack:
+
+    def __init__(self):
+        self.data = []
+    
+    def push(self, value):
+        self.data.append(value)
+    
+    def pop(self):
+        return self.data.pop()
+    
+    def __str__(self):
+        return str(self.data)
+
+my_stack = MyStack()
+
+"データを順にpushする"
+my_stack.push("a")
+print(my_stack)
+my_stack.push("b")
+print(my_stack)
+my_stack.push("c")
+print(my_stack)
+
+#データを順にpopsし、val1~val3に代入する
+val1 = my_stack.pop()
+print(my_stack)
+val2 = my_stack.pop()
+print(my_stack)
+val3 = my_stack.pop()
+print(my_stack)
+
+#取り出したデートを確認する
+print(val1, val2, val3)
