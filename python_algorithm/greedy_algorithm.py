0ef50972ed6a393715abdc9012aca0e5812adce4
saito4 <hiroto.saitou1201@gmail.com>
2025-01-09 14:49:15 +0900

    add_test

--- python_algorithm/greedy_algorithm.py
+++ python_algorithm/greedy_algorithm.py
@@ -2,16 +2,16 @@
 # コインの種類は、25円、10円、5円、1円
 
 def min_coins(amount, coins):
-    coins.sort(reverse=True) #コインを大きい順にソート
-    result = []
-    for coin in coins:
-        while amount >= coin:
-            amount -= coin
-            result.append(coin)
-    return result
+    coins.sort(reverse=True)    # コインを大きい順にソート
+    result = []                 # 結果として必要なコインを保存するリスト
+    for coin in coins:          # コインを順に処理する
+        while amount >= coin:   # amountが現在のコインの額以上なら
+            amount -= coin      # amountからそのコインの額を引く
+            result.append(coin) # 結果リストにそのコインを追加
+    return result               # 結果リストを返す
 
 # テスト
-amount = 93
+amount = 867
 coins = [25, 10, 5, 1]
 result = min_coins(amount, coins)
 print(f"金額{amount}を払うのに必要なコイン: {result} (枚数: {len(result)})")
