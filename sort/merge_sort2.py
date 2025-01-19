45533dce36c5167209759df368bab3925db86b63
saito4 <hiroto.saitou1201@gmail.com>
2025-01-19 18:45:36 +0900

    merge_sort 修正

--- sort/merge_sort2.py
+++ sort/merge_sort2.py
@@ -34,17 +34,36 @@ def merge(my_list, start_idx, mid_idx, end_idx):
     while left_idx < left_count:
         my_list[idx] = left_copy[left_idx]
         idx += 1
+        left_idx += 1
     
     while right_idx < right_count:
         my_list[idx] = right_copy[right_idx]
         idx += 1
         right_idx += 1
 
-data = [1, 2, 0, 3, 8, 4, 5, 8, 1, 9, 10]
-n = len(data)
-start_idx = 0
-end_idx = len(data) - 1
-mid_idx = (start_idx + end_idx) // 2
-merge(data, 0, mid_idx, n-1)
-print(data)
+# data = [1, 2, 0, 3, 8, 4, 5, 8, 1, 9, 10]
+# n = len(data)
+# start_idx = 0
+# end_idx = len(data) - 1
+# mid_idx = (start_idx + end_idx) // 2
+# merge(data, 0, mid_idx, n-1)
+# print(data)
     
+def merge_sort(my_list, start_idx, end_idx):
+    # start_idx < end_idxではない場合、部分リストの長さが一以下なので終了
+    if not (start_idx < end_idx):
+        return 
+
+    # 中央のインデックスを求める
+    mid_idx = (start_idx + end_idx) // 2
+
+    # 再帰的に分割する
+    merge_sort(my_list, start_idx, mid_idx)
+    merge_sort(my_list, mid_idx + 1, end_idx)
+
+    # ソートされた二つのデータ列をソートしながらマージする
+    merge(my_list, start_idx, mid_idx, end_idx)
+
+data = [1, 6, 3, 4, 5, 7, 0, 2]  
+merge_sort(data, 0, len(data) -1)
+print(data)
