6f3526ee61d968abc6db9bee1ba12ba2e5ce105d
saito4 <hiroto.saitou1201@gmail.com>
2025-01-24 11:48:05 +0900

    hash table

--- associative array/hash.py
+++ associative array/hash.py
@@ -6,5 +6,43 @@ def hash_func(text):
         hash_num += ascii_letters.index(c)
     return hash_num
 
-hash_val = hash_func("abc")
-print(hash_val)
+class Node:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+    
+    def __str__(self):
+        return "{key}:{value}".format(key=self.key, value=self.value)
+
+class Hashtable:
+
+    def __init__(self):
+        self.size = 100
+        self.data = [None] * self.size
+    
+    def set(self, key, value):
+        hash_key = hash_func(key) % self.size
+        self.data[hash_key] = Node(key, value)
+    
+    def get(self, key):
+        hash_key = hash_func(key) % self.size
+        return self.data[hash_key]
+    
+    def delete(self, key):
+        hash_key = hash_func(key) % self.size
+        self.data[hash_key] = None
+    
+    def __str__(self):
+        result = ""
+        for idx, node in enumerate(self.data):
+            if node:
+                result += str(idx) + ":" + str(node) + "\n"
+        return result
+
+map = Hashtable()
+map.set("Tanaka", "tanaka@example.com")
+map.set("Ootani", "ootani@example.com")
+print(map)
+
+mail = map.get("Tanaka")
+print("mail:", mail)
