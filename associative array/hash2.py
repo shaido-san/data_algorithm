3e312bfc4131258ecea892298e8e8f5128135860
saito4 <hiroto.saitou1201@gmail.com>
2025-01-25 20:08:52 +0900

    open address 途中

---
+++ associative array/hash2.py
@@ -0,0 +1,57 @@
+from string import ascii_letters
+
+def hash_func(text):
+    hash_num = 0
+    for c in text:
+        hash_num += ascii_letters.index(c)
+    return hash_num
+
+class Node:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+    
+    def __str__(self):
+        return "{key}:{value}".format(key=self.key, value=self.value)
+
+class Hashtable:
+
+    def __init__(self):
+        self.size = 100
+        self.data = [None] * self.size
+    
+    def set(self, key, value):
+        hash_key = hash_func(key) % self.size
+        while hash_key < self.size:
+            node = self.data[hash_key]
+            if node is None:
+                self.data[hash_key] = Node(key, value)
+                return
+            
+            # ハッシュの再計算
+            hash_key += 1
+        
+        print("空きが見つかりませんでした")
+    
+    def get(self, key):
+        hash_key = hash_func(key) % self.size
+        return self.data[hash_key]
+    
+    def delete(self, key):
+        hash_key = hash_func(key) % self.size
+        self.data[hash_key] = None
+    
+    def __str__(self):
+        result = ""
+        for idx, node in enumerate(self.data):
+            if node:
+                result += str(idx) + ":" + str(node) + "\n"
+        return result
+
+map = Hashtable()
+map.set("Tanaka", "tanaka@example.com")
+map.set("Ootani", "ootani@example.com")
+print(map)
+
+mail = map.get("Tanaka")
+print("mail:", mail)
