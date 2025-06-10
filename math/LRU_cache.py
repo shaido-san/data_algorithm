b418d0d985bbd036f624542798fce8e3be81d5d0
saito4 <hiroto.saitou1201@gmail.com>
2025-06-10 23:08:32 +0900

    LRUキャッシュ

---
+++ math/LRU_cache.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)  # 最近使ったので最後に移動
+        return self.cache[key]
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)  # 一番古いの捨てる
+
+# 実行例
+cache = LRUCache(2)
+cache.put(1, 1)
+cache.put(2, 2)
+print(cache.get(1))  # 1
+cache.put(3, 3)      # 2が捨てられる
+print(cache.get(2))  # -1（消えた）
